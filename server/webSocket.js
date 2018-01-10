53a961aaa934fd1796849bccb0314b3c08e4b9f4
Andy Dam <andy@andydam.com>
2018-01-09 20:19:54 -0800

    (feat) add database and websocket message handling

--- server/webSocket.js
+++ server/webSocket.js
@@ -1,17 +1,77 @@
 const WebSocket = require('ws');
 
-const onMessage = (ws, wss, data) => {
-  // if not JSON stringified error handle
-  const message = JSON.parse(data); // do something with data from client
-  console.log('recieved from client', data);
-  ws.send(JSON.stringify(message));
+const db = require('../database');
 
+const updateEveryoneElse = (ws, wss, data) => {
   wss.clients.forEach((client) => {
     if (client !== ws && client.readyState === WebSocket.OPEN) {
-      // send a message to all the other clients connected
-      client.send(`recieved message: ${JSON.stringify(message)} from another client`);
+      client.send(data);
     }
   });
+};
+
+const onMessage = (ws, wss, data) => {
+  let message;
+  try {
+    message = JSON.parse(data);
+  } catch (e) {
+    return ws.send(JSON.stringify({
+      code: 400,
+      message: e.message,
+    }));
+  }
+
+  switch (message.method) {
+    case 'GETMESSAGES':
+      db
+        .getMessages()
+        .then(messages =>
+          ws.send(JSON.stringify({
+            code: 200,
+            message: 'Request success',
+            method: 'GETMESSAGES',
+            data: messages,
+          })))
+        .catch(err =>
+          ws.send(JSON.stringify({
+            code: 400,
+            message: err.message,
+            method: 'GETMESSAGES',
+          })));
+      break;
+    case 'POSTMESSAGE':
+      db
+        .postMessage(message.data.text)
+        .then(dbData =>
+          updateEveryoneElse(
+            ws,
+            wss,
+            JSON.stringify({
+              code: 200,
+              message: 'New message',
+              method: 'NEWMESSAGE',
+              data: {
+                id: dbData.rows[0].id,
+                text: dbData.rows[0].text,
+                createdAt: dbData.rows[0].createdAt,
+              },
+            }),
+          ))
+        .then(() =>
+          ws.send(JSON.stringify({
+            code: 201,
+            message: 'Post success',
+            method: 'POSTMESSAGE',
+          })))
+        .catch(err =>
+          ws.send(JSON.stringify({
+            code: 400,
+            message: err.message,
+            method: 'POSTMESSAGE',
+          })));
+      break;
+    default:
+  }
 };
 
 const onConnect = (ws, wss) => {
