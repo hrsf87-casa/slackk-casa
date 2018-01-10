1d54df45adb6ffdb902519f75fb4043edb18b1f0
chasekaylee <chaseklee990@gmail.com>
2018-01-10 10:15:52 -0800

    (refactor) Modulize sendMessage ws to be in index.js socketHelpers

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect, sendMessage } from '../socket_helpers/websocket_helpers';
+import { connect, sendMessage } from '../socketHelpers';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,14 +7,13 @@ export default class App extends React.Component {
     this.state = {
       messages: [],
       // users: [],
-      ws: {},
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
-    // TODO: CHANGE TO PRODUCTION SERVER location.origin.replace(/^http/, 'wss')
+    // TODO: CHANGE TO PRODUCTION SERVER location.origin.replace(/^http/, 'ws')
     let server = 'wss://slackk-casa.herokuapp.com/';
 
     // connect to the websocket server
@@ -23,26 +22,20 @@ export default class App extends React.Component {
 
   // sends the input field value as a json object to the server socket
   handleClick() {
-    let msg = {
-      code: 200,
-      message: 'sending message to server',
-      method: 'POSTMESSAGE',
-      data: {
-        text: this.textInput.value,
-      },
-    };
-    sendMessage(this.state.ws, JSON.stringify(msg));
+    sendMessage(this.textInput.value);
   }
-
 
   render() {
     return (
       <div>
         {this.state.testText}
-        <input type="text" ref={(input) => { this.textInput = input; }} />
-        <button onClick={this.handleClick}>
-          Send!
-        </button>
+        <input
+          type="text"
+          ref={(input) => {
+            this.textInput = input;
+          }}
+        />
+        <button onClick={this.handleClick}>Send!</button>
         {JSON.stringify(this.state.messages)}
       </div>
     );
