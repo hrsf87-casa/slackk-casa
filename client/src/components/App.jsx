11678e25835dc81194629fa95ecc4b67f8b19960
Austin Shin <shinaustin@gmail.com>
2018-01-10 23:03:52 -0800

    (feat) set up client side ui

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,169 @@
 import React from 'react';
 import { connect, sendMessage } from '../socketHelpers';
 import MessageList from './MessageList.jsx';
+import { Button } from 'reactstrap';
+import NavBar from './NavBar.jsx';
+import Body from './Body.jsx';
+import { Container, FormGroup, Label, Input } from 'reactstrap';
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      messages: [],
+      messages: [
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'helllsoadfoasdfoaskdfkoasdhelllsoadfoasdfoaskdfkoashelllsoadfoasdfoaskdfkoas',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        {
+          id: 1230,
+          text: 'hellls131221331',
+        },
+        { id: 12341, text: 'this is a message style me' },
+        { id: 12341, text: 'this is a message style me2' },
+        { id: 12341, text: 'this is a message style me3' },
+        { id: 12341, text: 'this is a message style me4' },
+        { id: 12341, text: 'this is a message style me5' },
+        { id: 12341, text: 'this is a message style me6' },
+        { id: 12341, text: 'this is a message style me7' },
+        { id: 12341, text: 'this is a message style me8' },
+      ],
       // users: [],
+      query: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    let server = location.origin.replace(/^http/, 'ws');
+    // let server = location.origin.replace(/^http/, 'ws');
+    let server = 'ws://127.0.0.1:3000';
 
     // connect to the websocket server
     connect(server, this);
@@ -20,21 +171,37 @@ export default class App extends React.Component {
 
   // sends the input field value as a json object to the server socket
   handleClick() {
-    sendMessage(this.textInput.value);
+    sendMessage(this.state.query);
+  }
+
+  handleChange(event) {
+    this.setState({
+      query: event.target.value,
+    });
   }
 
   render() {
     let { messages } = this.state;
     return (
       <div>
-        <input
-          type="text"
-          ref={(input) => {
-            this.textInput = input;
-          }}
-        />
-        <button onClick={() => this.handleClick()}>Send!</button>
-        <MessageList messages={messages} />
+        {/* <Container fluid className="nav-container"> */}
+        <NavBar />
+
+        {/* </Container> */}
+
+        <Body messages={messages} />
+        <div className="input-container">
+
+          <FormGroup className="text-area">
+            <Label for="exampleText">Text Area</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+
+          {/* <input type="text" className="chat-input" onChange={this.handleChange} /> */}
+        </div>
+        <button className="chat-send-button" onClick={() => this.handleClick()}>
+          Send!
+        </button>
       </div>
     );
   }
