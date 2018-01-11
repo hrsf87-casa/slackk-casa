5e245d1cf2ce7c757d405e577aa30eb8ce658908
chasekaylee <chaseklee990@gmail.com>
2018-01-11 12:10:14 -0800

    (feat) Implement send message on enter press

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,6 +15,7 @@ export default class App extends React.Component {
       query: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   componentDidMount() {
@@ -24,22 +25,26 @@ export default class App extends React.Component {
     connect(server, this);
   }
 
-  // sends the input field value as a json object to the server socket
-  handleClick() {
-    console.log('username: ', this.props.location.state.username);
-    console.log('password: ', this.props.location.state.password);
-    sendMessage(this.state.query);
-  }
-
   handleChange(event) {
     this.setState({
       query: event.target.value,
     });
-    console.log(this.state.query);
+  }
+
+  handleKeyPress(event) {
+    // on key press enter send message and reset text box
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage(this.state.query);
+      // resets text box to blank string
+      this.setState({
+        query: '',
+      });
+    }
   }
 
   render() {
-    let { messages } = this.state;
+    let { messages, query } = this.state;
     return (
       <div>
         <NavBar />
@@ -47,12 +52,16 @@ export default class App extends React.Component {
         <div className="input-container">
           <FormGroup className="text-area">
             <Label for="exampleText">Text Area</Label>
-            <Input type="textarea" name="text" id="exampleText" onChange={this.handleChange} />
+            <Input
+              value={query}
+              type="textarea"
+              name="text"
+              id="exampleText"
+              onChange={this.handleChange}
+              onKeyPress={this.handleKeyPress}
+            />
           </FormGroup>
         </div>
-        <button className="chat-send-button" onClick={() => this.handleClick()}>
-          Send!
-        </button>
       </div>
     );
   }
