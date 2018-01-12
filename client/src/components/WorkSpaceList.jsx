03b1ca2cdd980c18cb6f5fa7b1a41378ebd65cfc
chasekaylee <chaseklee990@gmail.com>
2018-01-12 12:24:58 -0800

    (feat) Create workspaceList comp / createWS post req

---
+++ client/src/components/WorkSpaceList.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+import WorkSpaceEntry from './WorkSpaceEntry.jsx';
+
+export default class WorkSpaceList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      workSpaceQuery: '',
+      createFail: false,
+      createStatus: 'Failed to create workspace',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleFail = this.handleFail.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  createWorkSpace() {
+    let { getWorkSpaces } = this.props;
+    let { workSpaceQuery, createFail } = this.state;
+    this.setState({ createFail: false });
+    fetch('/workspaces', {
+      method: 'POST',
+      body: JSON.stringify({ workSpace: workSpaceQuery }),
+      headers: { 'content-type': 'application/json' },
+    })
+      .then(resp => (resp.status === 201 ? getWorkSpaces() : this.setState({ createFail: true })))
+      .catch(console.error);
+  }
+
+  handleClick() {
+    this.createWorkSpace();
+  }
+
+  handleFail() {
+    this.setState({ createFail: false });
+  }
+
+  handleChange(event) {
+    this.setState({ workSpaceQuery: event.target.value });
+  }
+
+  render() {
+    let { workSpaces } = this.props;
+    let { createFail, createStatus } = this.state;
+    return (
+      <div>
+        <h3> WorkSpaces </h3>
+        {workSpaces.map(workSpace => (
+          <WorkSpaceEntry workSpace={workSpace} handleFail={this.handleFail} key={workSpace.id} />
+        ))}
+        <input type="text" placeholder="workspace name.." onChange={this.handleChange} />
+        <button onClick={this.handleClick}> + </button>
+        <br />
+        <br />
+        {createFail ? <Alert color="danger"> {createStatus} </Alert> : undefined}
+      </div>
+    );
+  }
+}
